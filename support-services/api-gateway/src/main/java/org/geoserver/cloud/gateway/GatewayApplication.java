1d1ec079f2b7acff84d5916f8dad19b7514788dd
Gabriel Roldan <gabriel.roldan@gmail.com>
2020-08-19 06:14:41 -0300

    experimenting with circuit breaker in gateway

--- support-services/api-gateway/src/main/java/org/geoserver/cloud/gateway/GatewayApplication.java
+++ support-services/api-gateway/src/main/java/org/geoserver/cloud/gateway/GatewayApplication.java
@@ -4,22 +4,42 @@
  */
 package org.geoserver.cloud.gateway;
 
+import java.time.Duration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+
 @SpringBootApplication
 @Configuration
 public class GatewayApplication {
 
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(GatewayApplication.class).run(args);
-    }
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(GatewayApplication.class).run(args);
+	}
 
-    public @Bean RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes().build();
-    }
+	public @Bean RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes().build();
+	}
+
+	public @Bean Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		TimeLimiterConfig timeLimiter = TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(1)).build();
+		CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.ofDefaults();
+		
+		return factory -> factory.configureDefault(id -> //
+		new Resilience4JConfigBuilder(id)//
+				.circuitBreakerConfig(circuitBreakerConfig)//
+				.timeLimiterConfig(timeLimiter)//
+				.build()//
+		);
+	}
 }
