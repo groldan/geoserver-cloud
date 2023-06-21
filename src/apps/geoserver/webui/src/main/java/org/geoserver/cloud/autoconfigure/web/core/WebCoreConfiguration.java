869d59f6965b8faf276512bf487fd5736b98e4fe
Jose Macchi <jose.macchi@camptocamp.com>
2023-06-21 16:31:20 -0300

    WPS service integration (config, beans, etc). Documentation on how to setup datadir. Enabled TestWFSPost servlet in context of WebUI service.

--- src/apps/geoserver/webui/src/main/java/org/geoserver/cloud/autoconfigure/web/core/WebCoreConfiguration.java
+++ src/apps/geoserver/webui/src/main/java/org/geoserver/cloud/autoconfigure/web/core/WebCoreConfiguration.java
@@ -13,11 +13,13 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.vfny.geoserver.wfs.servlets.TestWfsPost;
 
 @Configuration(proxyBeanMethods = true)
 @ImportResource( //
         reader = FilteringXmlBeanDefinitionReader.class, //
         locations = { //
+            "jar:gs-wfs-.*!/applicationContext.xml",
             "jar:gs-web-core-.*!/applicationContext.xml#name="
                     + WebCoreConfiguration.EXCLUDED_BEANS_PATTERN //
         })
@@ -29,6 +31,10 @@ public class WebCoreConfiguration {
         return new GeoServerWicketServlet();
     }
 
+    public @Bean TestWfsPost testWfsPostServlet() {
+        return new TestWfsPost();
+    }
+
     /** Register the {@link WicketServlet} */
     public @Bean ServletRegistrationBean<GeoServerWicketServlet>
             geoServerWicketServletRegistration() {
@@ -36,6 +42,15 @@ public class WebCoreConfiguration {
         ServletRegistrationBean<GeoServerWicketServlet> registration;
         registration =
                 new ServletRegistrationBean<GeoServerWicketServlet>(servlet, "/web", "/web/*");
+
+        return registration;
+    }
+
+    /** Register the {@link TestWfsPost servlet} */
+    public @Bean ServletRegistrationBean<TestWfsPost> wfsTestServletRegistration() {
+        TestWfsPost servlet = testWfsPostServlet();
+        ServletRegistrationBean<TestWfsPost> registration;
+        registration = new ServletRegistrationBean<TestWfsPost>(servlet, "/TestWfsPost");
 
         return registration;
     }
